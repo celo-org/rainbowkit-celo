88c19f2944b8764cf809986e6b1155d1d2b833c8
Aaron <aaron@clabs.co>
2022-09-30 14:43:18 +0200

    Add Demo with Syntaxhighlighting

--- apps/example/next.config.js
+++ apps/example/next.config.js
@@ -1,35 +1,38 @@
 const withTM = require("next-transpile-modules")(["@celo/rainbowkit-celo"]);
+const withMDX = require("@next/mdx")()
+module.exports = withMDX(
+  withTM({
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack: (config, { webpack }) => {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        child_process: false,
+        readline: false,
+        // crypto: false,
+        // http: false,
+        // https: false,
+        // url: false,
+        // zlib: false,
+        // stream: false,
+        // tls: false,
+      }
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^electron$/ })
+      )
+      return config
+    },
 
-module.exports = withTM({
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config, { webpack }) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      child_process: false,
-      readline: false,
-      // crypto: false,
-      // http: false,
-      // https: false,
-      // url: false,
-      // zlib: false,
-      // stream: false,
-      // tls: false,
-    };
-    config.plugins.push(
-      new webpack.IgnorePlugin({ resourceRegExp: /^electron$/ })
-    );
-    return config;
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "/",
-      },
-    ];
-  },
-});
+    async rewrites() {
+      return [
+        {
+          source: "/:path*",
+          destination: "/",
+        },
+      ]
+    },
+  })
+)
--- apps/example/pages/_app.tsx
+++ apps/example/pages/_app.tsx
@@ -26,7 +26,7 @@ const wagmiClient = createClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider chains={chains} coolMode={true}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
