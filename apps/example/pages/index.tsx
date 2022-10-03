8052deed3a669ece18483f50aaa7fd2684d0aadd
Aaron DeRuvo <aaron@clabs.co>
2022-10-03 15:29:13 +0200

    Add Demo with Syntaxhighlighting (#3)
    * Add Demo with Syntaxhighlighting
    
    * build the package

--- apps/example/pages/index.tsx
+++ apps/example/pages/index.tsx
@@ -1,8 +1,51 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import SyntaxHighlighter from "react-syntax-highlighter";
+
+const code = `import { RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import celoGroups from "@celo/rainbowkit-celo/lists"
+import { Alfajores, Mainnet } from "@celo/rainbowkit-celo/chains";
+
+const { chains, provider } = configureChains(
+  [Alfajores, Mainnet],
+  [jsonRpcProvider({ rpc: (chain) => ({ http: chain.rpcUrls.default }) })]
+);
+
+const connectors = celoGroups({chains})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+export default function Wrap() {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <YourApp/>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+function YourApp() {
+  return <ConnectButton />;
+};
+`
+
+export function Demo() {
+return <div className={styles.code}>
+  <SyntaxHighlighter language="typescript" >
+    {code}
+  </SyntaxHighlighter>
+  </div>
+}
+
 
 export const YourApp = () => {
   return <ConnectButton />;
@@ -10,15 +53,15 @@ export const YourApp = () => {
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Celo Config for RainbowKit</title>
         <meta name="description" content="RainbowKit with Celo" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <h2>Celo 🌈 RainbowKit</h2>
+        <h2 className={styles.title}>Celo 🌈 RainbowKit</h2>
+        <Demo />
         <ConnectButton />
       </main>
 
@@ -32,7 +75,7 @@ const Home: NextPage = () => {
           💾
         </a>
       </footer>
-    </div>
+      </>
   );
 };
 
