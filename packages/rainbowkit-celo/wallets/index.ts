f4f199ac1c7362885c25c68192ba00c47e22ef39
Aaron <aaron@clabs.co>
2023-01-30 11:40:12 -0800

    fix missing js extension

--- packages/rainbowkit-celo/wallets/index.ts
+++ packages/rainbowkit-celo/wallets/index.ts
@@ -1,6 +1,5 @@
-export { default as Valora } from "./valora";
-export { default as CeloWallet } from "./celo-wallet";
-export { default as CeloTerminal } from "./celo-terminal";
-// export { default as CeloExtension } from "./celo-extension";
-export { default as CeloDance } from "./celo-dance";
+export { default as Valora } from "./valora.js";
+export { default as CeloWallet } from "./celo-wallet.js";
+export { default as CeloTerminal } from "./celo-terminal.js";
+export { default as CeloDance } from "./celo-dance.js";
 
