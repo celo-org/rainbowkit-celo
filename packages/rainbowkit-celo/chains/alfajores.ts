ae5f559a7568a983b57dce168ebeeb6e3293bedb
Aaron <aaron.deruvo@clabs.co>
2023-02-17 13:46:04 -0800

    upgrade to rainbowkit 11 wagmi 11 typescript 4.9.4
    Add wagmi/chains --
    get celo chain data from wagmi/chains where possible

--- packages/rainbowkit-celo/chains/alfajores.ts
+++ packages/rainbowkit-celo/chains/alfajores.ts
@@ -1,32 +1,9 @@
 import { Chain } from '@rainbow-me/rainbowkit';
-
+import { celoAlfajores } from '@wagmi/chains';
 const Alfajores: Chain = {
-  id: 44787,
-  name: 'Alfajores',
-  network: 'Alfajores Testnet',
+  ...celoAlfajores,
   iconUrl: 'https://rainbowkit-with-celo.vercel.app/icons/alfajores.svg',
   iconBackground: '#fff',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'CELO',
-    symbol: 'A-CELO',
-  },
-  rpcUrls: {
-    default: {
-        http: ['https://alfajores-forno.celo-testnet.org']
-      },
-  },
-  blockExplorers: {
-    default: { name: 'Celo Explorer', url: 'https://explorer.celo.org/alfajores' },
-    etherscan: { name: 'CeloScan', url: 'https://alfajores.celoscan.io/' },
-  },
-  contracts: {
-    multicall3: {
-      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
-      blockCreated: 14569001
-    }
-  },
-  testnet: true,
 };
 
 export default Alfajores;
--- packages/rainbowkit-celo/chains/celo.ts
+++ packages/rainbowkit-celo/chains/celo.ts
@@ -1,36 +1,9 @@
 import { Chain } from '@rainbow-me/rainbowkit';
-
+import { celo } from "@wagmi/chains"
 const Celo: Chain = {
-  id: 42220,
-  name: 'Celo Mainnet',
-  network: 'Celo Mainnet',
+  ...celo,
   iconUrl: 'https://rainbowkit-with-celo.vercel.app/icons/celo.svg',
   iconBackground: '#fff',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'CELO',
-    symbol: 'CELO',
-  },
-  rpcUrls: {
-    default: {http: ['https://forno.celo.org']},
-    infura: {
-      http: ["https://celo-mainnet.infura.io/v3/"]
-    },
-    public: {
-      http: ['https://forno.celo.org']
-    },
-  },
-  blockExplorers: {
-    default: { name: 'Celo Explorer', url: 'https://explorer.celo.org/mainnet' },
-    etherscan: { name: 'CeloScan', url: 'https://celoscan.io' },
-  },
-  contracts: {
-    multicall3: {
-      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
-      blockCreated: 13112599
-    },
-  },
-  testnet: false,
 };
 
 export default Celo;
