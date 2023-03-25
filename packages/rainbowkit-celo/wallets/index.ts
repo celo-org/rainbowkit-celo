2f193b791f43e802e49ca65e751e3f6451f73f8a
Kerem Noras <kerem@noras.tech>
2023-03-25 15:47:43 +0300

    feat(rainbowkit-celo): add support for celo-extension wallet type in rainbowkit-celo package

--- packages/rainbowkit-celo/wallets/index.ts
+++ packages/rainbowkit-celo/wallets/index.ts
@@ -2,4 +2,4 @@ export { default as Valora } from "./valora.js";
 export { default as CeloWallet } from "./celo-wallet.js";
 export { default as CeloTerminal } from "./celo-terminal.js";
 export { default as CeloDance } from "./celo-dance.js";
-
+export { default as CeloExtension } from "./celo-extension.js";
