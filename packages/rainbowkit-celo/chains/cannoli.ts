3dff1c3340bc455e6bc49452f82b52a57226478a
Aaron <aaron.deruvo@clabs.co>
2023-04-19 15:13:46 -0500

    Add cannoli Config

---
+++ packages/rainbowkit-celo/chains/cannoli.ts
@@ -0,0 +1,36 @@
+import { Chain } from '@rainbow-me/rainbowkit';
+const Cannoli: Chain = {
+  id: 17_323,
+  name: 'Cannoli',
+  network: 'celo-cannoli',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'CELO',
+    symbol: 'C-CELO',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://forno.cannoli.celo-testnet.org'],
+    },
+    public: {
+      http: ['https://forno.cannoli.celo-testnet.org'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Celo Explorer',
+      url: 'https://explorer.celo.org/cannoli',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0x5Acb0aa8BF4E8Ff0d882Ee187140713C12BF9718',
+      blockCreated: 87429,
+    },
+  },
+  testnet: true,
+  iconUrl: 'https://rainbowkit-with-celo.vercel.app/icons/cannoli.svg',
+  iconBackground: '#FCF6F1',
+};
+
+export default Cannoli;
--- packages/rainbowkit-celo/chains/index.ts
+++ packages/rainbowkit-celo/chains/index.ts
@@ -1,3 +1,4 @@
 export { default as Alfajores } from './alfajores.js';
+export { default as Cannoli } from './cannoli.js';
 export { default as Celo } from './celo.js';
 export { default as Baklava } from './baklava.js';
