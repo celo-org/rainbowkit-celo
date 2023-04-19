40750139fc9fc74b84f71b8fa830566ca997cd0d
Aaron <aaron.deruvo@clabs.co>
2023-04-19 15:13:46 -0500

    Add cannoli Config

--- apps/example/pages/_app.tsx
+++ apps/example/pages/_app.tsx
@@ -7,10 +7,10 @@ import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import celoGroups from "@celo/rainbowkit-celo/lists"
-import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
+import { Alfajores, Celo, Cannoli } from "@celo/rainbowkit-celo/chains";
 
 const { chains, provider } = configureChains(
-  [Alfajores, Celo],
+  [Alfajores, Celo, Cannoli],
   [jsonRpcProvider({ rpc: (chain) => ({ http: chain.rpcUrls.default.http[0] }) })]
 );
 
