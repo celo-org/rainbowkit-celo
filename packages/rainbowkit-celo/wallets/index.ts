51ff7e28295abc7ad1ebba1aaf1b27d40048b35c
Aaron <aaron.deruvo@clabs.co>
2023-05-11 13:37:22 -0500

    remove old wallets

--- packages/rainbowkit-celo/wallets/index.ts
+++ packages/rainbowkit-celo/wallets/index.ts
@@ -1,5 +1,3 @@
 export { default as Valora } from "./valora.js";
 export { default as CeloWallet } from "./celo-wallet.js";
 export { default as CeloTerminal } from "./celo-terminal.js";
-export { default as CeloDance } from "./celo-dance.js";
-
