64a8b4991c7f34828b22308397c662587160614d
Aaron <aaron.deruvo@clabs.co>
2023-06-27 11:32:27 -0500

    wip"

--- apps/example/pages/feecurrency.tsx
+++ apps/example/pages/feecurrency.tsx
@@ -1,32 +1,37 @@
 import { NextPage } from "next"
 import { useCallback } from "react"
-import {mainnet } from 'viem/chains'
-import { celo } from "@celo/viem-tools/chains";
-import { useWalletClient } from 'wagmi'
+import { celo } from 'viem/chains'
+import { useSendTransaction, useWalletClient } from 'wagmi'
 
-import { createWalletClient, http } from 'viem'
+import { SendTransactionParameters, createWalletClient, http } from 'viem'
 
 
 
 export const FeeCurrency: NextPage = () => {
 
-  const walletClient = createWalletClient({chain: mainnet, transport: http()})
+  const walletClient = createWalletClient({chain: celo, transport: http()})
 
-  walletClient.sendTransaction({feeCurrency:
-    '0x00000',
-    wrong: true, invalid: '1',
+  walletClient.sendTransaction({
+    feeCurrency: '0x00000',
     account: '0x00000',
-    chainId: celo.id, nonce: 0,
-    to: '0x00000',
-    value: '0x00000'
+    nonce: 0,
+    to: '0x00000'
   })
-
 
   const client = useWalletClient({chainId:celo.id})
 
+  const tx: SendTransactionParameters<typeof celo> = {
+    account: '0x00000',
+    feeCurrency: "0x1929393yu2u73732",
+    to: '0x20bh12'
+  }
+
   useCallback(() => {
-    client.data?.sendTransaction<typeof celo>({chain: celo, feeCurrency: '0x00000', foo: 'bar'})
-  }, [client])
+
+    client.data?.sendTransaction(tx)
+  }, [client, tx])
+
+
 
   return <>
 
