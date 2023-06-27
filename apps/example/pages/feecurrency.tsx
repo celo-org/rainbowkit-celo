1ac412545b7c8c2a9ddb0a7e42b6263835025ea6
Aaron <aaron.deruvo@clabs.co>
2023-06-27 10:26:07 -0500

    upgrade deps

--- apps/example/pages/feecurrency.tsx
+++ apps/example/pages/feecurrency.tsx
@@ -1,3 +1,34 @@
+import { NextPage } from "next"
+import { useCallback } from "react"
+import {mainnet } from 'viem/chains'
+import { celo } from "@celo/viem-tools/chains";
+import { useWalletClient } from 'wagmi'
+
+import { createWalletClient, http } from 'viem'
 
 
 
+export const FeeCurrency: NextPage = () => {
+
+  const walletClient = createWalletClient({chain: mainnet, transport: http()})
+
+  walletClient.sendTransaction({feeCurrency:
+    '0x00000',
+    wrong: true, invalid: '1',
+    account: '0x00000',
+    chainId: celo.id, nonce: 0,
+    to: '0x00000',
+    value: '0x00000'
+  })
+
+
+  const client = useWalletClient({chainId:celo.id})
+
+  useCallback(() => {
+    client.data?.sendTransaction<typeof celo>({chain: celo, feeCurrency: '0x00000', foo: 'bar'})
+  }, [client])
+
+  return <>
+
+  </>
+}
