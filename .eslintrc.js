40d8ec3295b2468b24b289a79442a79a08d98a5f
Leszek Stachowski <leszek.stachowski@clabs.co>
2024-01-25 15:54:15 +0100

    yarn4 migration (#102)
    * yarn4 migration
    * add CODEOWNERS and PR template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
   root: true,
   settings: {
     next: {
